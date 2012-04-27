090ba14c21976c667ccd3e3aaf751c0fbc009e2d
cpaterra_cp <SND\cpaterra_cp>
2012-04-26 18:59:48 -0700

    Added files missing from source (were in source package, though).

---
+++ Blog/Components/Business/Constants.vb
@@ -0,0 +1,60 @@
+﻿'
+' DotNetNuke® - http://www.dotnetnuke.com
+' Copyright (c) 2002-2012
+' by DotNetNuke Corporation
+'
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+' documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+' the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+' to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+'
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+' of the Software.
+'
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+' DEALINGS IN THE SOFTWARE.
+'
+
+Public Class Constants
+
+    Public Enum TagMode
+        ShowNoUsage = 0
+        ShowDailyUsage = 1
+        ShowWeeklyUsage = 2
+        ShowMonthlyUsage = 3
+        ShowTotalUsage = 4
+    End Enum
+
+    Public Const ContentTypeName As String = "DNN_Blog" ' change to DNN_Blog_Entry
+
+    ''' <summary>
+    ''' A recommended limit for a meta page title for SEO purposes.
+    ''' </summary>
+    Public Const SeoTitleLimit As Integer = 64
+
+    ''' <summary>
+    ''' A recommended limit for a meta page description for SEO purposes.
+    ''' </summary>
+    Public Const SeoDescriptionLimit As Integer = 150
+
+    ''' <summary>
+    ''' A recommended limit for meta page keywords for SEO purposes.
+    ''' </summary>
+    Public Const SeoKeywordsLimit As Integer = 15
+
+    Public Const SharedResourceFileName As String = "~/DesktopModules/Blog/App_LocalResources/SharedResources.ascx.resx"
+
+    Public Const ModuleCacheKeyPrefix As String = "DNN_Blog_"
+
+    Public Const VocabTermsCacheKey As String = "Vocabulary_Terms_"
+
+    Public Const PortalSuffixCacheKey As String = "Portal_"
+
+    Public Const VocabularySuffixCacheKey As String = "Vocabulary_"
+
+    Public Const ContentItemTermsCacheKey As String = "Portal_Terms_"
+
+End Class
---
+++ Blog/Components/Controllers/TermController.vb
@@ -0,0 +1,66 @@
+﻿'
+' DotNetNuke® - http://www.dotnetnuke.com
+' Copyright (c) 2002-2012
+' by DotNetNuke Corporation
+'
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+' documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+' the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+' to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+'
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+' of the Software.
+'
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+' DEALINGS IN THE SOFTWARE.
+'
+
+Imports System
+Imports DotNetNuke.Modules.Blog.Data
+Imports DotNetNuke.Common.Utilities
+Imports DotNetNuke.Entities.Host
+
+
+Namespace Business
+
+    Public Class TermController
+
+        Public Function GetTermsByContentType(portalId As Integer, moduleId As Integer, vocabularyId As Integer) As List(Of TermInfo)
+            'DotNetNuke.Common.Requires.PropertyNotNegative("portalId", "", portalId)
+            'DotNetNuke.Common.Requires.PropertyNotNegative("moduleId", "", moduleId)
+            Dim colTerms As List(Of TermInfo) = DirectCast(DataCache.GetCache(Constants.ModuleCacheKeyPrefix + Constants.VocabTermsCacheKey + Constants.PortalSuffixCacheKey + portalId.ToString()), List(Of TermInfo))
+
+            If colTerms Is Nothing Then
+                Dim timeOut As Integer = 20 * Convert.ToInt32(Host.PerformanceSetting)
+
+                colTerms = CBO.FillCollection(Of TermInfo)(DataProvider.Instance().GetTermsByContentType(portalId, Content.GetContentTypeID(), moduleId, vocabularyId))
+
+                If timeOut > 0 And colTerms IsNot Nothing Then
+                    DataCache.SetCache(Constants.ModuleCacheKeyPrefix + Constants.VocabTermsCacheKey + Constants.PortalSuffixCacheKey + portalId.ToString(), colTerms, TimeSpan.FromMinutes(timeOut))
+                End If
+            End If
+            Return colTerms
+        End Function
+
+        Public Function GetTermsByContentItem(contentItemId As Integer, vocabularyId As Integer) As List(Of TermInfo)
+            'DotNetNuke.Common.Requires.PropertyNotNegative("contentItemId", "", contentItemId);
+            Dim colTerms As List(Of TermInfo) = DirectCast(DataCache.GetCache(Constants.ModuleCacheKeyPrefix + Constants.ContentItemTermsCacheKey + contentItemId.ToString() + Constants.VocabularySuffixCacheKey), List(Of TermInfo))
+
+            If colTerms Is Nothing Then
+                Dim timeOut As Integer = 20 * Convert.ToInt32(Host.PerformanceSetting)
+
+                colTerms = CBO.FillCollection(Of TermInfo)(DataProvider.Instance().GetTermsByContentItem(contentItemId, vocabularyId))
+
+                If timeOut > 0 And colTerms IsNot Nothing Then
+                    DataCache.SetCache(Constants.ModuleCacheKeyPrefix + Constants.ContentItemTermsCacheKey + contentItemId.ToString() + Constants.VocabularySuffixCacheKey, colTerms, TimeSpan.FromMinutes(timeOut))
+                End If
+            End If
+            Return colTerms
+        End Function
+
+    End Class
+
+End Namespace
