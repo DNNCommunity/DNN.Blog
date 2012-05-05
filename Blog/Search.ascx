f41a6dd298bf24cdfae6fab97a6f70362b83053a
cpaterra_cp <SND\cpaterra_cp>
2012-05-05 11:23:25 -0700

    Converted search definition to form pattern (removed tables), added new folder to move some classes into, removed recent comments from project and it's module definition.

--- Blog/Search.ascx
+++ Blog/Search.ascx
@@ -1,26 +1,16 @@
 <%@ Control Language="vb" AutoEventWireup="false" CodeBehind="Search.ascx.vb" Inherits="DotNetNuke.Modules.Blog.Search" %>
-<table id="Table1" cellspacing="1" cellpadding="1" width="100%" border="0">
- <tr>
-  <td>
-   <asp:DropDownList ID="cboBlogSelect" runat="server" Width="100%" CssClass="NormalTextBox" />
-  </td>
- </tr>
- <tr>
-  <td>
-   <asp:TextBox ID="txtSearch" runat="server" CssClass="NormalTextBox" Width="100%" EnableViewState="False" />
-  </td>
- </tr>
- <tr>
-  <td>
-   <asp:RadioButtonList ID="optSearchType" runat="server" CssClass="SearchOptions" RepeatDirection="Horizontal" AutoPostBack="False">
-    <asp:ListItem Value="Keyword" Selected="True" ResourceKey="optSearchType_Keyword" />
-    <asp:ListItem Value="Phrase" ResourceKey="optSearchType_Phrase" />
-   </asp:RadioButtonList>
-  </td>
- </tr>
- <tr>
-  <td align="right">
-   <asp:Button ID="btnSearch" runat="server" CssClass="Normal" resourcekey="btnSearch" Text=" " />
-  </td>
- </tr>
-</table>
+<div class="dnnblogSearch dnnForm dnnClear">
+    <div class="dnnFormItem">
+        <asp:DropDownList ID="cboBlogSelect" runat="server" />
+    </div>
+    <div class="dnnFormItem">
+        <asp:TextBox ID="txtSearch" runat="server" EnableViewState="False" />
+    </div>
+    <div class="dnnFormItem">
+        <asp:RadioButtonList ID="optSearchType" runat="server" CssClass="dnnFormRadioButtons" RepeatDirection="Horizontal" AutoPostBack="False">
+            <asp:ListItem Value="Keyword" Selected="True" ResourceKey="optSearchType_Keyword" />
+            <asp:ListItem Value="Phrase" ResourceKey="optSearchType_Phrase" />
+        </asp:RadioButtonList>
+        <asp:Button ID="btnSearch" runat="server" resourcekey="btnSearch" Text=" " />
+    </div>
+</div>
--- Blog/Search.ascx.vb
+++ Blog/Search.ascx.vb
@@ -25,103 +25,87 @@ Imports DotNetNuke.Services.Exceptions
 Imports DotNetNuke.Common.Globals
 
 Partial Class Search
- Inherits BlogModuleBase
+    Inherits BlogModuleBase
 
-#Region " Private Members "
- Private BlogID As Integer = -1
- Private m_PersonalBlogID As Integer
-#End Region
+#Region "Private Members"
 
-#Region "Controls"
+    Private BlogID As Integer = -1
+    Private m_PersonalBlogID As Integer
+
 #End Region
 
 #Region "Event Handlers"
- Private Sub Page_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-  Try
-   If Not Request.Params("BlogID") Is Nothing Then
-    BlogID = CType(Request.Params("BlogID"), Integer)
-   End If
-   If Not Page.IsPostBack Then
-    Dim objBlogs As New BlogController
-    Dim list As ArrayList
 
-    m_PersonalBlogID = BlogSettings.PageBlogs
-    If m_PersonalBlogID <> -1 Then
-     BlogID = m_PersonalBlogID
-    End If
-    If m_PersonalBlogID <> -1 Then
-     Dim m_oBlog As New BlogInfo
-     m_oBlog = objBlogs.GetBlog(m_PersonalBlogID)
-     list = New ArrayList
-     list.Add(m_oBlog)
-    Else
-     list = objBlogs.ListBlogs(PortalId, -1, DotNetNuke.Security.PortalSecurity.IsInRole(PortalSettings.AdministratorRoleId.ToString()))
-    End If
+    Protected Sub Page_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try
+            If Not Request.Params("BlogID") Is Nothing Then
+                BlogID = CType(Request.Params("BlogID"), Integer)
+            End If
+            If Not Page.IsPostBack Then
+                Dim objBlogs As New BlogController
+                Dim list As ArrayList
 
-    If list.Count < 2 Then
-     cboBlogSelect.Visible = False
-    Else
-    cboBlogSelect.DataTextField = "Title"
-    cboBlogSelect.DataValueField = "BlogID"
-    cboBlogSelect.DataSource = list
-    cboBlogSelect.DataBind()
-    If m_PersonalBlogID = -1 Then
-     cboBlogSelect.Items.Insert(0, New System.Web.UI.WebControls.ListItem(Localization.GetString("lstViewAllBlogs", LocalResourceFile), "-1"))
-    End If
-    If Not cboBlogSelect.Items.FindByValue(BlogID.ToString()) Is Nothing Then
-     cboBlogSelect.Items.FindByValue(BlogID.ToString()).Selected = True
-    End If
-    End If
+                m_PersonalBlogID = BlogSettings.PageBlogs
+                If m_PersonalBlogID <> -1 Then
+                    BlogID = m_PersonalBlogID
+                End If
+                If m_PersonalBlogID <> -1 Then
+                    Dim m_oBlog As New BlogInfo
+                    m_oBlog = objBlogs.GetBlog(m_PersonalBlogID)
+                    list = New ArrayList
+                    list.Add(m_oBlog)
+                Else
+                    list = objBlogs.ListBlogs(PortalId, -1, DotNetNuke.Security.PortalSecurity.IsInRole(PortalSettings.AdministratorRoleId.ToString()))
+                End If
 
-    If Not Request.Params("SearchType") Is Nothing Then
-     If Request.Params("SearchType") = "Phrase" Then
-      optSearchType.Items.FindByValue("Phrase").Selected = True
-     Else
-      optSearchType.Items.FindByValue("Keyword").Selected = True
-     End If
-    End If
-    If Not Request.Params("Search") Is Nothing Then
-     txtSearch.Text = Request.Params("Search")
-    End If
-    txtSearch.Attributes.Add("onkeypress", "if (event.keyCode == 13) {document.forms[0]." & btnSearch.ClientID & ".click();event.keyCode = 0; return false;}")
-   End If
+                If list.Count < 2 Then
+                    cboBlogSelect.Visible = False
+                Else
+                    cboBlogSelect.DataTextField = "Title"
+                    cboBlogSelect.DataValueField = "BlogID"
+                    cboBlogSelect.DataSource = list
+                    cboBlogSelect.DataBind()
+                    If m_PersonalBlogID = -1 Then
+                        cboBlogSelect.Items.Insert(0, New System.Web.UI.WebControls.ListItem(Localization.GetString("lstViewAllBlogs", LocalResourceFile), "-1"))
+                    End If
+                    If Not cboBlogSelect.Items.FindByValue(BlogID.ToString()) Is Nothing Then
+                        cboBlogSelect.Items.FindByValue(BlogID.ToString()).Selected = True
+                    End If
+                End If
 
-  Catch exc As Exception
-   ProcessModuleLoadException(Me, exc)
-  End Try
- End Sub
+                If Not Request.Params("SearchType") Is Nothing Then
+                    If Request.Params("SearchType") = "Phrase" Then
+                        optSearchType.Items.FindByValue("Phrase").Selected = True
+                    Else
+                        optSearchType.Items.FindByValue("Keyword").Selected = True
+                    End If
+                End If
+                If Not Request.Params("Search") Is Nothing Then
+                    txtSearch.Text = Request.Params("Search")
+                End If
+                txtSearch.Attributes.Add("onkeypress", "if (event.keyCode == 13) {document.forms[0]." & btnSearch.ClientID & ".click();event.keyCode = 0; return false;}")
+            End If
+
+        Catch exc As Exception
+            ProcessModuleLoadException(Me, exc)
+        End Try
+    End Sub
+
+    Protected Sub btnSearch_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSearch.Click
+        StartSearch()
+    End Sub
+
 #End Region
 
 #Region "Private Methods"
- Private Sub btnSearch_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSearch.Click
-  StartSearch()
- End Sub
 
- Private Sub StartSearch()
-  Dim sURL As String = Utility.AddTOQueryString(NavigateURL(), "Search", txtSearch.Text)
-  sURL = Utility.AddTOQueryString(sURL, "SearchType", optSearchType.SelectedValue)
-  If Not cboBlogSelect.SelectedItem Is Nothing Then
-   If Int32.Parse(cboBlogSelect.SelectedItem.Value) > -1 Then sURL = sURL & "&BlogID=" & cboBlogSelect.SelectedItem.Value
-  End If
-  Response.Redirect(sURL)
- End Sub
-#End Region
-
-#Region " Web Form Designer Generated Code "
-
- 'This call is required by the Web Form Designer.
- <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
-
- End Sub
-
- 'NOTE: The following placeholder declaration is required by the Web Form Designer.
- 'Do not delete or move it.
- Private designerPlaceholderDeclaration As System.Object
-
-    Private Overloads Sub Page_Init(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Init
-        'CODEGEN: This method call is required by the Web Form Designer
-        'Do not modify it using the code editor.
-        InitializeComponent()
+    Private Sub StartSearch()
+        Dim sURL As String = Utility.AddTOQueryString(NavigateURL(), "Search", txtSearch.Text)
+        sURL = Utility.AddTOQueryString(sURL, "SearchType", optSearchType.SelectedValue)
+        If Not cboBlogSelect.SelectedItem Is Nothing Then
+            If Int32.Parse(cboBlogSelect.SelectedItem.Value) > -1 Then sURL = sURL & "&BlogID=" & cboBlogSelect.SelectedItem.Value
+        End If
+        Response.Redirect(sURL)
     End Sub
 
 #End Region
