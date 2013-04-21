d3648b6618b414a38cec3ad6509f13307ca4b394
Peter Donker <peter@bring2mind.net>
2013-04-21 10:11:46 +0200

    Included sharrre for social sharing

--- Templates/_default/template.js
+++ Templates/_default/template.js
@@ -5,6 +5,27 @@
    $dialogComment.dialog('open');
    return false;
   });
+  $('#sharrre').sharrre({
+   share: {
+    googlePlus: true,
+    facebook: true,
+    twitter: true
+   },
+   template: '<a href="#" class="blogicon-facebook socialicons facebook"></a><a href="#" class="blogicon-twitter socialicons twitter"></a><a href="#" class="blogicon-google-plus socialicons googleplus"></a>',
+   enableHover: false,
+   enableTracking: true,
+   render: function (api, options) {
+    $(api.element).on('click', '.twitter', function () {
+     api.openPopup('twitter');
+    });
+    $(api.element).on('click', '.facebook', function () {
+     api.openPopup('facebook');
+    });
+    $(api.element).on('click', '.googleplus', function () {
+     api.openPopup('googlePlus');
+    });
+   }
+  });
  });
 } (jQuery, window.Sys));
 function karmaToggle(btn, blogId, commentId, karma, confirmation) {
