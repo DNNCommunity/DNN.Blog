9855705e0750c8194072592ac88c4e508437c2c4
donker <peter@bring2mind.net>
2014-08-25 13:21:15 +0200

    WI_24026 Fix to PostEdit dnn_ContentItems_Tags

--- Components/Integration/NotificationController.vb
+++ Components/Integration/NotificationController.vb
@@ -17,8 +17,6 @@
 ' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 ' DEALINGS IN THE SOFTWARE.
 '
-Option Strict On
-Option Explicit On
 
 Imports DotNetNuke.Services.Social.Notifications
 Imports DotNetNuke.Modules.Blog.Entities
