15ea69a2c1075a379a8513b04eaccded412096be
Peter Donker <peter@bring2mind.net>
2024-05-09 11:50:12 +0200

    Release

--- Server/OpenUrlRewriter/My Project/AssemblyInfo.vb
+++ Server/OpenUrlRewriter/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.5.5")>
-<Assembly: AssemblyFileVersion("6.5.5")>
+<Assembly: AssemblyVersion("6.5.6")>
+<Assembly: AssemblyFileVersion("6.5.6")>
